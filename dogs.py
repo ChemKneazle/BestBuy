099a57c0d87745228fcb2a271f6cc0a9d2b7aae9
DESKTOP-4DAHM18\ChemKneazle <ChemKneazle@gmail.com>
2017-09-25 22:56:17 -0600

    Updated dogs.py to add or delete dogs from list.

--- dogs.py
+++ dogs.py
@@ -1,16 +1,25 @@
 dogs = ['Scruffy', 'Fido', 'Rover', 'Pete']
-for dog in dogs:
-    print(dog)
 
-print("I'm gonna go pick up some dogs")
+def print_dogs_in_tow(dogs_list):
+    dogs_to_print = 'Dogs in tow: '
 
+    for dog in dogs_list:
+        dogs_to_print += dog + ', '
+
+    dogs_to_print = dogs_to_print[0:-2] + '\n'
+    print(dogs_to_print)
+
+
+print_dogs_in_tow(dogs)
+
+print("I'm'a go pick up some dogg")
 dogs.append('Beatrix')
-for dog in dogs:
-    print(dog)
+print_dogs_in_tow(dogs)
 
-print("I'm gonna output a range of dogs")
+print("I'm'a go drop off my first dogg %s." % dogs[0])
+dogs.pop(0)
+print_dogs_in_tow(dogs)
 
-specialdogs = dogs[-1:1]
-
-for dog in specialdogs:
-    print(dog)
+print("I'm'a go drop off my last dogg %s." % dogs[-1])
+dogs.pop(-1)
+print_dogs_in_tow(dogs)
